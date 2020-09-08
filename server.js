14f7db82ec7a317bb36cd5f1040b58b7c478bf7b
Carlos <bwaycarl@gmail.com>
2020-09-08 16:05:19 -0400

    Moved server.js file.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = 3000;
 // Sets up the Express app to handle data parsing ___________
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static(__dirname));
 
 fs.readFile ("db/db.json", "utf8", (err, data) => {
     if (err) throw err;
@@ -24,18 +24,24 @@ fs.readFile ("db/db.json", "utf8", (err, data) => {
 app.get("/api/notes", function(req, res) {
     res.json(notes);
 });
-    //* POST `/api/notes` - Should receive a new note to save on the request body, 
-    //add it to the `db.json` file, and then return the new note to the client.
+
+//* POST `/api/notes` - Should receive a new note to save on the request body, 
+//add it to the `db.json` file, and then return the new note to the client.
+
+app.post("/api/notes", function(req, res) {
     
-    app.post("/api/notes", function(req, res) {
-        
-        let newNote = req.body;
-        notes.push(newNote);
-        updateDb();
-        return console.log(newNote);
-        
-      });
+    let newNote = req.body;
+    notes.push(newNote);
+    updateDb();
+    return console.log("New note: " + newNote.title);
+    
     });
+});
+
+//* DELETE `/api/notes/:id` - Should receive a query parameter 
+//containing the id of a note to delete.
+
+
     
 // Routes _______________________________________
 
